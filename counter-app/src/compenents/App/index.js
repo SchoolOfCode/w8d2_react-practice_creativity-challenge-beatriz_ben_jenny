6eab57817bc6939dfe79b42a33c810d771830b50
Beatriz Bravo <beatrici25@gmail.com>
2022-01-11 16:11:44 +0000

    m

--- counter-app/src/compenents/App/index.js
+++ counter-app/src/compenents/App/index.js
@@ -6,6 +6,7 @@ import Timer from '../Timer';
 import InputTime from '../InputTime';
 import Counter from '../Counter';
 
+
 function App() {
 
 
--- counter-app/src/compenents/InputTime/index.js
+++ counter-app/src/compenents/InputTime/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from "react";
 
 function InputTime() {
-    return (
-        <div>
-            <p>input time</p>
-        </div>
-    )
+  const [inputValue, setInputValue] = useState(0);
+
+  function handleInput(input) {
+    setInputValue(input);
+  }
+function startTime(time) {
+
+    
 }
 
-export default InputTime
+
+  return (
+    <div>
+      <input
+        value={inputValue}
+        type="Number"
+        placeholder="enter numbers of seconds"
+        onChange={(e) => {
+          handleInput(e.target.value);
+        }}
+      ></input>
+      <button onClick={startTime}>start</button>
+    </div>
+  );
+}
+
+export default InputTime;
