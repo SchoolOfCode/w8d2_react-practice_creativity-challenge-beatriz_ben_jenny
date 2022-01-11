cb58228748ec5e8b8582abea796f5fa87e2d6e4e
benmhcode <85193753+benmhcode@users.noreply.github.com>
2022-01-11 16:34:57 +0000

    countdown timer coded

--- counter-app/src/compenents/InputTime/index.js
+++ counter-app/src/compenents/InputTime/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 function InputTime() {
   const [inputValue, setInputValue] = useState(0);
@@ -6,14 +6,19 @@ function InputTime() {
   function handleInput(input) {
     setInputValue(input);
   }
-function startTime(time) {
-
-    
-}
-
+  function startTime() {
+    setCountdown(inputValue);
+  }
+  const [countdown, setCountdown] = useState(0);
+  useEffect(() => {
+    const timer =
+      countdown > 0 && setInterval(() => setCountdown(countdown - 1), 1000);
+    return () => clearInterval(timer);
+  }, [countdown]);
 
   return (
     <div>
+      <p id="timer-display">{countdown}</p>
       <input
         value={inputValue}
         type="Number"
@@ -22,7 +27,9 @@ function startTime(time) {
           handleInput(e.target.value);
         }}
       ></input>
-      <button onClick={startTime}>start</button>
+      <button id="start-button" onClick={startTime}>
+        start
+      </button>
     </div>
   );
 }
